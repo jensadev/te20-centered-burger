6cf1aaeca06d87f44045e0f8c5c7e6095f305a3f
Jens Andreasson <jensandreasson77@gmail.com>
2022-12-12 16:02:39 +0100

    fixed background color on nav, inner container

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,6 @@
-console.log('Ny fil i js/menu.js');
-
 const button = document.querySelector('.navbar__button');
 
 button.addEventListener('click', function(event) {
-    console.log(event);
     const menu = document.querySelector('.navbar__collapse');
-    console.log(menu);
     menu.classList.toggle('navbar__collapse--show');
 });
---
+++ readme.md
@@ -0,0 +1,2 @@
+http://parsons.problemsolving.io/puzzle/66af35c576e4426fac5f0b87b82f1e9f
+
